2a170bd1c68b05ffabeeb127bb03736190b9c085
kzhong678 <68789831+kzhong678@users.noreply.github.com>
2021-07-30 20:26:48 -0500

    Initial Upload

---
+++ YesWorkflow/OuterWorkflow.py
@@ -0,0 +1,20 @@
+# @BEGIN main
+
+# @BEGIN OpenRefine_Data_Cleaning
+# @IN raw_data.csv
+# @OUT cleaned_data.csv
+# @OUT OpenRefineOperationHistory.JSON
+# @END OpenRefine_Data_Cleaning
+
+# @BEGIN SQL_Validation
+# @IN cleaned_data.csv
+# @OUT validated_data.csv
+# @END SQL_Validation
+
+# @BEGIN Or2yw_Inner_Workflow_Modeling
+# @IN OpenRefineOperationHistory.JSON
+# @OUT InnerWorflow.pdf
+# @OUT InnerWorflow.png
+# @END Or2yw_Inner_Workflow_Modeling
+
+# @END main
